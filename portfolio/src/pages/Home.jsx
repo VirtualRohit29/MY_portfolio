bac4d36b7929eae33f37bb9e083b1f256d079fd7
VirtualRohit29 <rohitkr290704@gmail.com>
2025-07-03 00:06:21 +0530

    make reuseable css variables

---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+export const Home =()=>{
+   return <div className="min-h-screen">
+    {/* Theme Toggle*/}
+     
+    {/* Background Effects*/}
+    
+    
+    {/* Navbar*/}
+    {/* Main content*/}
+
+    {/* Footer*/}
+
+    
+   </div>
+};
