223656ddcde17ad5598736046fdce7364d717815
VirtualRohit29 <rohitkr290704@gmail.com>
2025-07-07 14:09:49 +0530

    theme toggle and backround created

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,15 +1,22 @@
-export const Home =()=>{
-   return <div className="min-h-screen">
+
+
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground";
+
+export const Home = () =>{
+   return (<div className="min-h-screen bg-background text-foreground overflow-x-hidden">
     {/* Theme Toggle*/}
-     
+       <ThemeToggle/>
     {/* Background Effects*/}
-    
+         
+    <StarBackground/>
     
     {/* Navbar*/}
     {/* Main content*/}
 
     {/* Footer*/}
-
+   
     
    </div>
+   );
 };
